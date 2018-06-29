e67fa97e094bfc0de1ea0745f297e023a018c311
Aiiko6 <33965737+Aiiko6@users.noreply.github.com>
2018-06-29 21:30:54 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,1 +1,10 @@
+const Discord = require("discord.js");
+const token = "NDYyMzMwOTI3NDMxOTQyMTQ2.DhgS0w.YiZzpeOrQrCoQyzLLaAMANCoWd8"
+var bot = new Discord.Client();
 
+bot.on("ready", function(){
+	bot.user.setGame("J'aime mon maitre :)");
+	console.log("le bot est lancé");
+})
+
+bot.login(token);
